fe40ccedf30782affd1f2681c8b97ecef89262d6
RicardoMG <richar.montoyagallego@gmail.com>
2022-06-06 14:47:05 -0500

    ultimos archivos

--- src/main/java/com/platzi/market/domain/service/ProductService.java
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -1,6 +1,5 @@
 package com.platzi.market.domain.service;
 
-
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,27 +13,26 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAll(){
+    public List<Product> getAll() {
         return productRepository.getAll();
     }
 
-    public Optional<Product> getProduct(int productId){
+    public Optional<Product> getProduct(int productId) {
         return productRepository.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId){
+    public Optional<List<Product>> getByCategory(int categoryId) {
         return productRepository.getByCategory(categoryId);
     }
 
-    public Product save(Product product){
+    public Product save(Product product) {
         return productRepository.save(product);
     }
 
-    public boolean delete(int productId){
+    public boolean delete(int productId) {
         return getProduct(productId).map(product -> {
             productRepository.delete(productId);
             return true;
-
         }).orElse(false);
     }
 }
